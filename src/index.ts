6185ea1123c865d0e010ec70e7d399f9031ce790
Elias Ylänen <elias.ylanen@gmail.com>
2023-09-08 22:14:16 +0300

    chore: watch code style with Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,38 @@
-import data from "./data.json" assert { type: "json" };
+import data from './data.json' assert { type: 'json' };
 
 interface Purchase {
-  readonly item: string;
-  readonly quantity: number;
-  readonly price: number;
+	readonly item: string;
+	readonly quantity: number;
+	readonly price: number;
 }
 
 interface Client {
-  readonly name: string;
-  readonly location: string;
-  readonly purchases: Array<Purchase>;
+	readonly name: string;
+	readonly location: string;
+	readonly purchases: Array<Purchase>;
 }
 
 const groupByLocation = (data: Client[]) => {
-  return data.reduce((acc, client) => {
-    const { location } = client;
+	return data.reduce(
+		(acc, client) => {
+			const { location } = client;
 
-    acc[location] = [...(acc?.[location] ?? []), client];
+			acc[location] = [...(acc?.[location] ?? []), client];
 
-    return acc;
-  }, {} as Record<string, Client[]>);
+			return acc;
+		},
+		{} as Record<string, Client[]>,
+	);
 };
 
 const pirkkaParhaatAsiakkaat = (data: Client[]) => {
-  return data.filter(({ purchases }) => {
-    return (
-      purchases.reduce((acc, { quantity, price }) => {
-        return acc + quantity * price;
-      }, 0) >= 100
-    );
-  });
+	return data.filter(({ purchases }) => {
+		return (
+			purchases.reduce((acc, { quantity, price }) => {
+				return acc + quantity * price;
+			}, 0) >= 100
+		);
+	});
 };
 
 const grouped = groupByLocation(pirkkaParhaatAsiakkaat(data));
